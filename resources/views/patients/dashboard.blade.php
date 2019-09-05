97572ddfa276dcee8ee532a878a1202ff7e2cf28
Dickens-odera <dickensodera9@gmail.com>
2019-09-05 17:46:03 +0300

    Separated the javascript links from the main index page

--- resources/views/patients/dashboard.blade.php
+++ resources/views/patients/dashboard.blade.php
@@ -2,3 +2,5 @@
 <body class="hold-transition skin-blue sidebar-mini">
 @include('admin.partials.includes.header')
 @include('admin.partials.includes.sidebar')
+
+@include('admin.partials.includes.js-links')
