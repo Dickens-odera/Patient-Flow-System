1543b3d9555ebb10a766075dfdb29729ae8cf1a2
Dickens-odera <dickensodera9@gmail.com>
2019-09-05 14:27:59 +0300

    Modified the login pages

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,4 +1,131 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>{{ config('app.name','Patient Flow System')}}</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{!! asset('login-page/Login_v1/images/icons/favicon.ico') !!}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/vendor/bootstrap/css/bootstrap.min.css') !!}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css') !!}">
+<!--===============================================================================================-->
+	<link rel="stylesheet') !!}" type="text/css" href="{! asset('login-page/Login_v1/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/vendor/css-hamburgers/hamburgers.min.css') !!}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/vendor/select2/select2.min.css') !!}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/css/util.css') !!}">
+	<link rel="stylesheet" type="text/css" href="{!! asset('login-page/Login_v1/css/main.css') !!}">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="{!! asset('login-page/Login_v1/images/logo/flow2.jpg" alt="IMG') !!}">
+				</div>
+
+                <form class="login100-form validate-form" method="post" action="{{ route('admin.login.submit') }}">
+                    {{ csrf_field() }}
+					<span class="login100-form-title">
+						{{ __('ADMIN LOGIN') }}
+					</span>
+
+					<div class="wrap-input100 validate-input $errors->has('email')? 'has-error':''">
+                        <input class="input100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus type="email" placeholder="Email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @enderror
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+                        <input class="input100 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" type="password" placeholder="Password" >
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+                    <!-- Remember me-->
+                        <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+					</div>
+					
+                    <!-- end remember me -->
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							{{ __('Login') }}
+						</button>
+					</div>
+
+					<div class="text-center p-t-12">
+						{{-- <span class="txt1">
+							{{ __('Forgot') }}
+						</span> --}}
+						<a class="txt2" href="#">
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif						</a>
+					</div>
+
+					<!-- <div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div> -->
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="{!! asset('login-page/Login_v1/vendor/jquery/jquery-3.2.1.min.js') !!}"></script>
+<!--===============================================================================================-->
+	<script src="{!! asset('login-page/Login_v1/vendor/bootstrap/js/popper.js') !!}"></script>
+	<script src="{!! asset('login-page/Login_v1/vendor/bootstrap/js/bootstrap.min.js') !!}"></script>
+<!--===============================================================================================-->
+	<script src="{!! asset('login-page/Login_v1/vendor/select2/select2.min.js') !!}"></script>
+<!--===============================================================================================-->
+	<script src="{!! asset('login-page/Login_v1/vendor/tilt/tilt.jquery.min.js') !!}"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="{!! asset('login-page/Login_v1/js/main.js') !!}"></script>
+
+</body>
+</html>
+<!-- 
 
 @section('content')
 <div class="container">
@@ -70,4 +197,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection -->
