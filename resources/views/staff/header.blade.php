99fa758ae455ca48cd9c352aa9602668651ac802
Dickens-odera <dickensodera9@gmail.com>
2019-11-03 15:35:19 +0300

    Modofied the pharmacist's dashboard

--- resources/views/staff/header.blade.php
+++ resources/views/staff/header.blade.php
@@ -225,25 +225,11 @@
                 <img src="{!! asset('admin/dist/img/user2-160x160.jpg') !!}" class="img-circle" alt="User Image">
 
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                  {{ Auth::user()->name }}
+                  <small>Pharmacist</small>
                 </p>
               </li>
               <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
--- resources/views/staff/sidebar.blade.php
+++ resources/views/staff/sidebar.blade.php
@@ -26,7 +26,7 @@
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
+            {{-- <li class="active treeview">
               <a href="#">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 <span class="pull-right-container">
@@ -37,8 +37,8 @@
                 <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
                 <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
               </ul>
-            </li>
-            <li class="treeview">
+            </li> --}}
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-files-o"></i>
                 <span>Layout Options</span>
@@ -52,31 +52,29 @@
                 <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
                 <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
               </ul>
-            </li>
-            <li>
+            </li> --}}
+            {{-- <li>
               <a href="pages/widgets.html">
                 <i class="fa fa-th"></i> <span>Widgets</span>
                 <span class="pull-right-container">
                   <small class="label pull-right bg-green">new</small>
                 </span>
               </a>
-            </li>
+            </li> --}}
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-pie-chart"></i>
-                <span>Charts</span>
+                <span>Transactions</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-                <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-                <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-                <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+                <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i>All</a></li>
+                <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Complete</a></li>
               </ul>
             </li>
-            <li class="treeview">
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
                 <span>UI Elements</span>
@@ -92,8 +90,8 @@
                 <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
                 <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
               </ul>
-            </li>
-            <li class="treeview">
+            </li> --}}
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-edit"></i> <span>Forms</span>
                 <span class="pull-right-container">
@@ -105,8 +103,8 @@
                 <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
                 <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
               </ul>
-            </li>
-            <li class="treeview">
+            </li> --}}
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-table"></i> <span>Tables</span>
                 <span class="pull-right-container">
@@ -117,8 +115,8 @@
                 <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
                 <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
               </ul>
-            </li>
-            <li>
+            </li> --}}
+            {{-- <li>
               <a href="pages/calendar.html">
                 <i class="fa fa-calendar"></i> <span>Calendar</span>
                 <span class="pull-right-container">
@@ -126,8 +124,8 @@
                   <small class="label pull-right bg-blue">17</small>
                 </span>
               </a>
-            </li>
-            <li>
+            </li> --}}
+            {{-- <li>
               <a href="pages/mailbox/mailbox.html">
                 <i class="fa fa-envelope"></i> <span>Mailbox</span>
                 <span class="pull-right-container">
@@ -136,8 +134,8 @@
                   <small class="label pull-right bg-red">5</small>
                 </span>
               </a>
-            </li>
-            <li class="treeview">
+            </li> --}}
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-folder"></i> <span>Examples</span>
                 <span class="pull-right-container">
@@ -155,8 +153,8 @@
                 <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
                 <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
               </ul>
-            </li>
-            <li class="treeview">
+            </li> --}}
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-share"></i> <span>Multilevel</span>
                 <span class="pull-right-container">
@@ -188,12 +186,12 @@
                 </li>
                 <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
               </ul>
-            </li>
-            <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
-            <li class="header">LABELS</li>
+            </li> --}}
+            {{-- <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li> --}}
+            {{-- <li class="header">LABELS</li>
             <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> --}}
           </ul>
         </section>
         <!-- /.sidebar -->
